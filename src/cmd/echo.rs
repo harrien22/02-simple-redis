36d4498d0ecbab8ad444984b058c83db1810971f
harrien <realmyfly@gmail.com>
2024-05-17 16:58:17 +0100

    add echo command

---
+++ src/cmd/echo.rs
@@ -0,0 +1,48 @@
+use super::{extract_args, validate_command, CommandExecutor};
+use crate::{
+    cmd::{CommandError, Echo},
+    RespArray, RespFrame,
+};
+
+#[allow(unused_variables)]
+impl CommandExecutor for Echo {
+    fn execute(self, backend: &crate::Backend) -> RespFrame {
+        RespFrame::BulkString(self.message.into())
+    }
+}
+
+impl TryFrom<RespArray> for Echo {
+    type Error = CommandError;
+    fn try_from(value: RespArray) -> Result<Self, Self::Error> {
+        validate_command(&value, &["echo"], 1)?;
+
+        let mut args = extract_args(value, 1)?.into_iter();
+        match args.next() {
+            Some(RespFrame::BulkString(key)) => Ok(Echo {
+                message: String::from_utf8(key.0.expect("Invalid message"))?,
+            }),
+            _ => Err(CommandError::InvalidArgument("Invalid key".to_string())),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::RespDecode;
+    use anyhow::Result;
+    use bytes::BytesMut;
+
+    #[test]
+    fn test_echo_from_resp_array() -> Result<()> {
+        let mut buf = BytesMut::new();
+        buf.extend_from_slice(b"*2\r\n$4\r\necho\r\n$5\r\nhello\r\n");
+
+        let frame = RespArray::decode(&mut buf)?;
+
+        let result: Echo = frame.try_into()?;
+        assert_eq!(result.message, "hello");
+
+        Ok(())
+    }
+}
